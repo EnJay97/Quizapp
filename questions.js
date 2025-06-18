564cf30c2d3c576a353b818703e7636a08c084a7
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-06-18 14:19:41 +0200

    added questions.js, cleared script.js

---
+++ questions.js
@@ -0,0 +1,48 @@
+
+let questions = [
+    {
+        "question": "Wie viele Kantone gibt es in der Schweiz?",
+        "answer_1": "12",
+        "answer_2": "24",
+        "answer_3": "26",
+        "answer_4": "30",
+        "right_answer": 3
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 
+    }
+
+]
