fde4d3d1cec1bab3cfc03785f9462d0ddd24c8bb
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-06-18 15:37:21 +0200

    added first code for first card, removed from html

--- questions.js
+++ questions.js
@@ -10,64 +10,64 @@ let questions = [
         "explanation": "Es gibt 20 sogenannte Vollkantone und 6 Halbkantone. Trotzdem zählt jeder als eigener Kanton."
     },
 
-    {
-        "question": "Woher stammt das Kürzel 'CH' für die Schweiz?",
-        "answer_1": "Lateinisch - Confoederatio Helvetica",
-        "answer_2": "Französich - Confédération Helvétique",
-        "answer_3": "Deutsch - Schweizerische Eidgenossenschaft",
-        "answer_4": "Italienisch - Confederazione Elvetica",
-        "right_answer": 1,
-        "explanation": "Latein wurde gewählt, weil es neutral ist und keine der Landessprachen bevorzugt."
-    },
-
-    {
-        "question": "Wie viele Landessprachen hat die Schweiz?",
-        "answer_1": "1",
-        "answer_2": "2",
-        "answer_3": "3",
-        "answer_4": "4",
-        "right_answer": 4,
-        "explanation": "In verschiedenen Teilen der Schweiz gibt es verschiedene Amtssprachen: Deutsch, Französisch, Italienisch, Rätoromanisch."
-    },
-
-    {
-        "question": "Was ist der 'Röschtigraben?'",
-        "answer_1": "Eine Autobahn durch die Berge",
-        "answer_2": "Die sprachliche Grenze zwischen Deutsch und Französisch",
-        "answer_3": "Die Grenze zwischen Stadt und Land",
-        "answer_4": "Ein historischer Festungsgraben",
-        "right_answer": 2,
-        "explanation": "Röschtigraben bezeichnet die kulturelle und sprachliche Grenze zwischen der deutschsprachigen und der französichsprachigen Schweiz. Es ist aber eher mit einem Augenzwinkern zu betrachten."
-    },
-
-    {
-        "question": "Welches Land grenzt nicht an die Schweiz",
-        "answer_1": "Belgien",
-        "answer_2": "Frankreich",
-        "answer_3": "Italien",
-        "answer_4": "Deutschland",
-        "right_answer": 1,
-        "explanation": "Die Schweiz hat 5 Nachbarländer. Deutschland, Frankreich, Italien, Liechtenstein und Österreich."
-    },
-
-    {
-        "question": "Was ist die Hauptstadt der Schweiz?",
-        "answer_1": "Zürich",
-        "answer_2": "Genf",
-        "answer_3": "Es gibt keine offizielle Hauptstadt",
-        "answer_4": "Bern",
-        "right_answer": 3,
-        "explanation": "Die Schweiz hat keine Hauptstadt, weil ihre Verfassung keine vorsieht. Bern ist Regierungssitz festgelegt."
-    }
-
-    {
-        "question": "Wie heißt der Berg auf dem Bild?",
-        "answer_1": "Monte San Salvatore",
-        "answer_2": "Mont Blanc",
-        "answer_3": "Lepontinische Alpen",
-        "answer_4": "Matterhorn",
-        "right_answer": 4,
-        "explanation": "Die Schweiz hat keine Hauptstadt, weil ihre Verfassung keine vorsieht. Bern ist Regierungssitz festgelegt."
-    }
-
-]
+//    {
+//        "question": "Woher stammt das Kürzel 'CH' für die Schweiz?",
+//        "answer_1": "Lateinisch - Confoederatio Helvetica",
+//        "answer_2": "Französich - Confédération Helvétique",
+//        "answer_3": "Deutsch - Schweizerische Eidgenossenschaft",
+//        "answer_4": "Italienisch - Confederazione Elvetica",
+//        "right_answer": 1,
+//        "explanation": "Latein wurde gewählt, weil es neutral ist und keine der Landessprachen bevorzugt."
+//    },
+//
+//    {
+//        "question": "Wie viele Landessprachen hat die Schweiz?",
+//        "answer_1": "1",
+//        "answer_2": "2",
+//        "answer_3": "3",
+//        "answer_4": "4",
+//        "right_answer": 4,
+//        "explanation": "In verschiedenen Teilen der Schweiz gibt es verschiedene Amtssprachen: Deutsch, Französisch, Italienisch, Rätoromanisch."
+//    },
+//
+//    {
+//        "question": "Was ist der 'Röschtigraben?'",
+//        "answer_1": "Eine Autobahn durch die Berge",
+//        "answer_2": "Die sprachliche Grenze zwischen Deutsch und Französisch",
+//        "answer_3": "Die Grenze zwischen Stadt und Land",
+//        "answer_4": "Ein historischer Festungsgraben",
+//        "right_answer": 2,
+//        "explanation": "Röschtigraben bezeichnet die kulturelle und sprachliche Grenze zwischen der deutschsprachigen und der französichsprachigen Schweiz. Es ist aber eher mit einem Augenzwinkern zu betrachten."
+//    },
+//
+//    {
+//        "question": "Welches Land grenzt nicht an die Schweiz",
+//        "answer_1": "Belgien",
+//        "answer_2": "Frankreich",
+//        "answer_3": "Italien",
+//        "answer_4": "Deutschland",
+//        "right_answer": 1,
+//        "explanation": "Die Schweiz hat 5 Nachbarländer. Deutschland, Frankreich, Italien, Liechtenstein und Österreich."
+//    },
+//
+//    {
+//        "question": "Was ist die Hauptstadt der Schweiz?",
+//        "answer_1": "Zürich",
+//        "answer_2": "Genf",
+//        "answer_3": "Es gibt keine offizielle Hauptstadt",
+//        "answer_4": "Bern",
+//        "right_answer": 3,
+//        "explanation": "Die Schweiz hat keine Hauptstadt, weil ihre Verfassung keine vorsieht. Bern ist Regierungssitz festgelegt."
+//    },
+//
+//    {
+//        "question": "Wie heißt der Berg auf dem Bild?",
+//        "answer_1": "Monte San Salvatore",
+//        "answer_2": "Mont Blanc",
+//        "answer_3": "Lepontinische Alpen",
+//        "answer_4": "Matterhorn",
+//        "right_answer": 4,
+//        "explanation": "Das Matterhorn ist 4.478 Meter hoch und gilt durch seine einzigartige Form als Wahrzeichen der Alpen."
+//    }
+//
+];
--- script.js
+++ script.js
@@ -1,2 +1,53 @@
-// Karten dynamisch erstellen
-//
+
+function init() {
+    renderCards();
+
+};
+
+init();
+
+// Karten rendern
+
+function renderCards(){
+    let contentRef = document.getElementById("content");
+    contentRef.innerHTML = "";
+
+    for (let i = 0; i < questions.length; i++) {
+        contentRef.innerHTML += `<img src="./img/matterhorn.jpg" class="card-img-top">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${questions[i].question}</h5>
+
+                                    <div class="card mb-2">
+                                        <div class="card-body">
+                                            <p>${questions[i].answer_1}</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="card mb-2">
+                                        <div class="card-body">
+                                            <p>${questions[i].answer_2}</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="card mb-2">
+                                        <div class="card-body">
+                                            <p>${questions[i].answer_3}</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="card mb-2">
+                                        <div class="card-body">
+                                            <p>${questions[i].answer_4}</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="question-footer">
+                                        <div>
+                                            <b>${i+1}</b> von <b>${questions.length}</b> Fragen
+                                        </div>
+                                        <div>
+                                            <button>nächste Frage</button>
+                                        </div>
+                                    </div>`
+}
+};
