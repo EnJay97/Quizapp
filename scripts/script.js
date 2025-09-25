b27db3a1d85097f7f533fc0dd9e541e7229245e1
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-09-25 17:04:23 +0200

    editet script

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,25 @@ let currentQuestion = 0;
 
 function init() {
     document.getElementById("allQuestions").innerHTML = questions.length;
-    document.getElementById("currentQuestion").innerHTML = currentQuestion +1;
-    document.getElementById("button").disabled = true;
     showCurrentQuestion();
 }
 
 function showCurrentQuestion() {
-    let question = questions[currentQuestion];
 
-    document.getElementById("questionText").innerHTML = question["question"];
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];
+    if(currentQuestion >= questions.length) {
+        //show Endscreen
+        document.getElementById("endscreen").style = '';
+        document.getElementById("question-body").style = 'display: none'
+    } else
+        {
+        let question = questions[currentQuestion];
+        document.getElementById("currentQuestion").innerHTML = currentQuestion+1;
+        document.getElementById("questionText").innerHTML = question["question"];
+        document.getElementById("answer_1").innerHTML = question["answer_1"];
+        document.getElementById("answer_2").innerHTML = question["answer_2"];
+        document.getElementById("answer_3").innerHTML = question["answer_3"];
+        document.getElementById("answer_4").innerHTML = question["answer_4"];
+    }
 }
 
 function answer(selection) {
@@ -34,5 +40,19 @@ function answer(selection) {
 }
 
 function nextQuestion() {
-    
+    currentQuestion++;
+    document.getElementById("button").disabled = true;
+    resetAnswerButtons();
+    showCurrentQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
