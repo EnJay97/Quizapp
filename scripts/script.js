fb3d1a901c27e7dcfd748ccf14c3682776289ba5
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-09-25 16:14:59 +0200

    added function answer, extended function showCurrentQuestion

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 
 let currentQuestion = 0;
+
 function init() {
     document.getElementById("allQuestions").innerHTML = questions.length;
+    document.getElementById("currentQuestion").innerHTML = currentQuestion +1;
+    document.getElementById("button").disabled = true;
     showCurrentQuestion();
 }
 
@@ -9,4 +12,27 @@ function showCurrentQuestion() {
     let question = questions[currentQuestion];
 
     document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`
+ 
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    }
+    else{
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById("button").disabled = false;
+}
+
+function nextQuestion() {
+    
 }
